e9b0f167cc4f912082ae67c478f02f42e031b03d
Alexander Gil <pando855@gmail.com>
2026-02-25 10:56:41 +0100

    release: sync helm chart version with app version

--- .ci/release.sh
+++ .ci/release.sh
@@ -7,6 +7,11 @@ vim ./Cargo.toml
 # update lock file
 cargo update -p robotlb
 VERSION=$(sed -n 's/^version = "\(.*\)"/\1/p' ./Cargo.toml | head -n1)
+
+# keep Helm chart metadata in sync with the application release
+sed -i -E "s/^version: .*/version: ${VERSION}/" ./helm/Chart.yaml
+sed -i -E "s/^appVersion: .*/appVersion: \"${VERSION}\"/" ./helm/Chart.yaml
+
 git cliff -t ${VERSION} -u -p CHANGELOG.md
 
 git add .
